e2750bd1d59dcc7eb4a3ef93fba3101aea160a9b
joeolamide <odulatejoseph@gmail.com>
2022-06-28 19:26:49 +0100

    Merge remote-tracking branch 'origin/master'  working on user log in details.

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:shop_app/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String? _token;
   DateTime? _expiryDate;
   String? _userid;
 
-  Future<void> signup(
-      String email, String password) async {
+
+  Future<void> _authenticate(String email, String password, String urlSegment) async{
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAOfGCUP9JZ4h_XgTIPpsl5j-sPKEE3OaU');
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+        'https://identitytoolkit.googleapis.com/v1/$urlSegment?key=AIzaSyAOfGCUP9JZ4h_XgTIPpsl5j-sPKEE3OaU');
+    try{
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null){
+        throw HttpException(responseData['error']['message']);
+      }
+    }catch(error) {
+      throw error;
+    }
+  }
+
+
+  Future<void> signup(String email, String password) async {
+   return _authenticate(email, password, 'accounts:signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'accounts:signInWithPassword');
   }
 }
